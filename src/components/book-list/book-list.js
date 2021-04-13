2b30f2734943e458dfef021f7596d24687d76930
Rafik Abdryakhimov <raaafik95@gmail.com>
2021-04-13 19:03:35 +0300

    add book-list component

---
+++ src/components/book-list/book-list.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import BookListItem from '../book-list-item';
+
+class BookList extends Component {
+    render() {
+        const { books } = this.props;
+        return (
+            <ul>
+                {
+                    books.map((book) => {
+                        return (
+                            <li>
+                                <BookListItem key={book.id} book={book} />
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        )
+    }
+}
+
+export default BookList;
