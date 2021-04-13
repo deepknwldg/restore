8996b1dcb42279c5af4f6e330470de608b8d84c7
Rafik Abdryakhimov <raaafik95@gmail.com>
2021-04-13 19:08:53 +0300

    add app comonent

---
+++ src/components/app/app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './app.css';
+import { Route, Switch } from 'react-router-dom';
+import { HomePage, CartPage } from '../pages';
+
+const App = () => {
+    return (
+        <Switch>
+            <Route
+                path="/"
+                component={HomePage}
+                exact
+            />
+            <Route
+                path="/cart"
+                component={CartPage}
+                exact
+            />
+        </Switch>
+    )
+};
+
+export default App;
