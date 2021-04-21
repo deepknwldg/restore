e2b979969faa0fb1d81cf337802297d44a3f754a
Rafik Abdryakhimov <raaafik95@gmail.com>
2021-04-21 15:40:56 +0300

    add reject

--- src/servises/bookstore-service.js
+++ src/servises/bookstore-service.js
@@ -20,8 +20,14 @@ export default class BookstoreService {
   ];
 
   getBooks() {
-    return new Promise((resolve) => {
-      setTimeout(() => resolve(this.data), 700);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (Math.random() > 0.75) {
+          reject(new Error("Something bad happened"));
+        } else {
+          resolve(this.data);
+        }
+      }, 700);
     });
   }
 }
