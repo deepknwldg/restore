215d635427357f7c246213dbc1e28271941518f9
Rafik Abdryakhimov <raaafik95@gmail.com>
2021-04-22 22:04:01 +0300

    add middleware

--- src/store.js
+++ src/store.js
@@ -1,6 +1,30 @@
-import { createStore, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reducer from "./reducers";
 
+const logMiddleware = ({ getState, dispatch }) => (next) => (action) => {
+  console.log(action.type, getState());
+  return next(action);
+};
+
+const stringMiddleware = () => (next) => (action) => {
+  if (typeof action === "string") {
+    return next({
+      type: action,
+    });
+  }
+  return next(action);
+};
+
+const store = createStore(
+  reducer,
+  applyMiddleware(stringMiddleware, logMiddleware)
+);
+
+store.dispatch("HELLO_WORLD");
+
+export default store;
+
+/*
 const stringEnhancer = (createStore) => (...args) => {
   const store = createStore(...args);
   const originalDispatch = store.dispatch;
@@ -28,7 +52,4 @@ const logEnhancer = (createStore) => (...args) => {
 
   return store;
 };
-
-const store = createStore(reducer, compose(stringEnhancer, logEnhancer));
-
-export default store;
+*/
